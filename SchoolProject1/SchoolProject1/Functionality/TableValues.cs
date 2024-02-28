797e742d6f2b0c16044c6bd94e68d12397b8793c
1399999 <mzhebrunov@gmail.com>
2024-02-27 22:31:38 -0500

    27.02.2024

---
+++ SchoolProject1/SchoolProject1/Functionality/TableValues.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolProject1.Functionality
+{
+    public class TableValues
+    {
+        public string Column1 { get; set; } = string.Empty;
+        public string Column2 { get; set; } = string.Empty;
+        public string Column3 { get; set; } = string.Empty;
+    }
+}
