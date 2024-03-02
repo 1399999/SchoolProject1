eedceff22c78230946ff6aad84ef9a3d5a0b3f46
1399999 <mzhebrunov@gmail.com>
2024-03-02 11:19:02 -0500

    01.03.24

---
+++ SchoolProject1/SchoolProject1/Functionality/StandardModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SchoolProject1.Functionality;
+
+public static class StandardModel
+{
+    public static bool IsSettings { get; set; }
+    public static TableValues TableValues { get; set; } = new();
+    public static int ColumnCount { get; set; } = 3;
+}
--- SchoolProject1/SchoolProject1/Functionality/TableValues.cs
+++ SchoolProject1/SchoolProject1/Functionality/TableValues.cs
@@ -1,9 +1,44 @@
-﻿namespace SchoolProject1.Functionality
+﻿namespace SchoolProject1.Functionality;
+public class TableValues1
 {
-    public class TableValues
-    {
-        public string Column1 { get; set; } = string.Empty;
-        public string Column2 { get; set; } = string.Empty;
-        public string Column3 { get; set; } = string.Empty;
-    }
+    public string Column1 { get; set; } = string.Empty;
+}
+
+public class TableValues2
+{
+    public string Column1 { get; set; } = string.Empty;
+    public string Column2 { get; set; } = string.Empty;
+}
+
+public class TableValues3
+{
+    public string Column1 { get; set; } = string.Empty;
+    public string Column2 { get; set; } = string.Empty;
+    public string Column3 { get; set; } = string.Empty;
+}
+
+public class TableValues4
+{
+    public string Column1 { get; set; } = string.Empty;
+    public string Column2 { get; set; } = string.Empty;
+    public string Column3 { get; set; } = string.Empty;
+    public string Column4 { get; set; } = string.Empty;
+}
+
+public class TableValues5
+{
+    public string Column1 { get; set; } = string.Empty;
+    public string Column2 { get; set; } = string.Empty;
+    public string Column3 { get; set; } = string.Empty;
+    public string Column4 { get; set; } = string.Empty;
+    public string Column5 { get; set; } = string.Empty;
+}
+
+public class TableValues
+{
+    public TableValues1 Table1 { get; set; } = new();
+    public TableValues2 Table2 { get; set; } = new();
+    public TableValues3 Table3 { get; set; } = new();
+    public TableValues4 Table4 { get; set; } = new();
+    public TableValues5 Table5 { get; set; } = new();
 }
